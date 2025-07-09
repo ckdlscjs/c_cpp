14f7221b4391eacc3f01511604122fbffb713536
선용 <ckdlscjs@naver.com>
2025-07-10 02:05:47 +0900

    #FIX 뷰행렬 열/행 반대로 한것 수정

--- C_CPP/PardCode11/std.h
+++ C_CPP/PardCode11/std.h
@@ -152,9 +152,9 @@ inline XMMATRIX GetMat_ViewMatrix(const XMFLOAT3 posCamera, const XMFLOAT3 posTa
 	XMVECTOR u = XMVector3Normalize(XMVector3Cross(j, w));
 	//up, y
 	XMVECTOR v = XMVector3Cross(w, u);
-	mat.r[0] = u;
-	mat.r[1] = v;
-	mat.r[2] = w;
+	mat.r[0] = XMVectorSet(XMVectorGetX(u), XMVectorGetX(v), XMVectorGetX(w), 0.0f);
+	mat.r[1] = XMVectorSet(XMVectorGetY(u), XMVectorGetY(v), XMVectorGetY(w), 0.0f);
+	mat.r[2] = XMVectorSet(XMVectorGetZ(u), XMVectorGetZ(v), XMVectorGetZ(w), 0.0f);
 	//-(Q dot u), -(Q dot v), -(Q dot w), 1.0f
 	//dot내적의 결과가 x요소에있음
 	mat.r[3] = XMVectorSet(
