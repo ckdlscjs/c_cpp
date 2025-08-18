c26401b56629f290dbd7f049e9dc42216ac0750f
선용 <ckdlscjs@naver.com>
2025-08-18 23:52:19 +0900

    #FEAT RasterTek16, FrusutmCulling, pitch/yaw 입력 추가

---
+++ C_CPP/RasterTek16/PositionClass.cpp
@@ -0,0 +1,156 @@
+#include "stdafx.h"
+#include "PositionClass.h"
+
+
+PositionClass::PositionClass()
+{
+}
+
+
+PositionClass::PositionClass(const PositionClass& other)
+{
+}
+
+
+PositionClass::~PositionClass()
+{
+}
+
+
+void PositionClass::SetFrameTime(float time)
+{
+	m_frameTime = time;
+}
+
+
+void PositionClass::GetRotationY(float& y)
+{
+	y = m_rotationY;
+}
+
+void PositionClass::GetRotationX(float& x)
+{
+	x = m_rotationX;
+}
+
+
+void PositionClass::TurnLeft(bool keydown)
+{
+	// 키를 누르면 카메라가 왼쪽으로 돌아는 속도가 증가합니다. 회전 속도를 늦추지 않으면.
+	if (keydown)
+	{
+		m_leftTurnSpeed += m_frameTime * 0.01f;
+
+		if (m_leftTurnSpeed > (m_frameTime * 0.15f))
+		{
+			m_leftTurnSpeed = m_frameTime * 0.15f;
+		}
+	}
+	else
+	{
+		m_leftTurnSpeed -= m_frameTime* 0.005f;
+
+		if (m_leftTurnSpeed < 0.0f)
+		{
+			m_leftTurnSpeed = 0.0f;
+		}
+	}
+
+	// 회전 속도를 사용하여 회전을 업데이트합니다.
+	m_rotationY -= m_leftTurnSpeed;
+	if (m_rotationY < 0.0f)
+	{
+		m_rotationY += 360.0f;
+	}
+}
+
+
+void PositionClass::TurnRight(bool keydown)
+{
+	// 키를 누르면 카메라가 오른쪽으로 회전하는 속도가 증가합니다. 회전 속도를 늦추지 않으면.
+	if (keydown)
+	{
+		m_rightTurnSpeed += m_frameTime * 0.01f;
+
+		if (m_rightTurnSpeed > (m_frameTime * 0.15f))
+		{
+			m_rightTurnSpeed = m_frameTime * 0.15f;
+		}
+	}
+	else
+	{
+		m_rightTurnSpeed -= m_frameTime* 0.005f;
+
+		if (m_rightTurnSpeed < 0.0f)
+		{
+			m_rightTurnSpeed = 0.0f;
+		}
+	}
+
+	// 회전 속도를 사용하여 회전을 업데이트합니다.
+	m_rotationY += m_rightTurnSpeed;
+	if (m_rotationY > 360.0f)
+	{
+		m_rotationY -= 360.0f;
+	}
+}
+
+void PositionClass::TurnUp(bool keydown)
+{
+	// 키를 누르면 카메라가 왼쪽으로 돌아는 속도가 증가합니다. 회전 속도를 늦추지 않으면.
+	if (keydown)
+	{
+		m_UpTurnSpeed += m_frameTime * 0.01f;
+
+		if (m_UpTurnSpeed > (m_frameTime * 0.15f))
+		{
+			m_UpTurnSpeed = m_frameTime * 0.15f;
+		}
+	}
+	else
+	{
+		m_UpTurnSpeed -= m_frameTime * 0.005f;
+
+		if (m_UpTurnSpeed < 0.0f)
+		{
+			m_UpTurnSpeed = 0.0f;
+		}
+	}
+
+	// 회전 속도를 사용하여 회전을 업데이트합니다.
+	m_rotationX -= m_UpTurnSpeed;
+	if (m_rotationX < 0.0f)
+	{
+		m_rotationX += 360.0f;
+	}
+}
+
+void PositionClass::TurnDown(bool keydown)
+{
+	// 키를 누르면 카메라가 왼쪽으로 돌아는 속도가 증가합니다. 회전 속도를 늦추지 않으면.
+	if (keydown)
+	{
+		m_DownTurnSpeed += m_frameTime * 0.01f;
+
+		if (m_DownTurnSpeed > (m_frameTime * 0.15f))
+		{
+			m_DownTurnSpeed = m_frameTime * 0.15f;
+		}
+	}
+	else
+	{
+		m_DownTurnSpeed -= m_frameTime * 0.005f;
+
+		if (m_DownTurnSpeed < 0.0f)
+		{
+			m_DownTurnSpeed = 0.0f;
+		}
+	}
+
+	// 회전 속도를 사용하여 회전을 업데이트합니다.
+	m_rotationX += m_DownTurnSpeed;
+	if (m_rotationX > 360.0f)
+	{
+		m_rotationX -= 360.0f;
+	}
+}
---
+++ C_CPP/RasterTek16/PositionClass.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class PositionClass
+{
+public:
+	PositionClass();
+	PositionClass(const PositionClass&);
+	~PositionClass();
+
+	void SetFrameTime(float);
+	void GetRotationY(float&);
+	void GetRotationX(float&);
+
+	void TurnLeft(bool);
+	void TurnRight(bool);
+	void TurnUp(bool);
+	void TurnDown(bool);
+
+private:
+	float m_frameTime = 0.0f;
+	float m_rotationY = 0.0f;
+	float m_leftTurnSpeed = 0.0f;
+	float m_rightTurnSpeed = 0.0f;
+	float m_rotationX = 0.0f;
+	float m_UpTurnSpeed = 0.0f;
+	float m_DownTurnSpeed = 0.0f;
+};
