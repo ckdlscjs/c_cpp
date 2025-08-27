8b9c5e52891c1b7bf66aa4857d15593ea5ad49ea
선용 <ckdlscjs@naver.com>
2025-08-27 16:11:14 +0900

    #FEAT RasterTek17, MultiTexture

---
+++ C_CPP/RasterTek17/ModelClass.h
@@ -0,0 +1,51 @@
+#pragma once
+
+class TextureArrayClass;
+
+class ModelClass
+{
+private:
+	struct VertexType
+	{
+		XMFLOAT3 position;
+		XMFLOAT2 texture;
+	};
+
+	struct ModelType
+	{
+		float x, y, z;
+		float tu, tv;
+		float nx, ny, nz;
+	};
+
+public:
+	ModelClass();
+	ModelClass(const ModelClass&);
+	~ModelClass();
+
+	bool Initialize(ID3D11Device*, char*, WCHAR*, WCHAR*, WCHAR*);
+	void Shutdown();
+	void Render(ID3D11DeviceContext*);
+
+	int GetIndexCount();
+	ID3D11ShaderResourceView** GetTextureArray();
+
+private:
+	bool InitializeBuffers(ID3D11Device*);
+	void ShutdownBuffers();
+	void RenderBuffers(ID3D11DeviceContext*);
+
+	bool LoadTextures(ID3D11Device*, WCHAR*, WCHAR*, WCHAR*);
+	void ReleaseTextures();
+
+	bool LoadModel(char*);
+	void ReleaseModel();
+
+private:
+	ID3D11Buffer* m_vertexBuffer = nullptr;
+	ID3D11Buffer* m_indexBuffer = nullptr;
+	int m_vertexCount = 0;
+	int m_indexCount = 0;
+	TextureArrayClass* m_TextureArray = nullptr;
+	ModelType* m_model = nullptr;
+};
---
+++ C_CPP/RasterTek17/multitextureps.hlsl
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: multitexture.ps
+////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////
+// GLOBALS //
+/////////////
+Texture2D shaderTextures[3]; //멀티텍스쳐를위해서 두장의 Texture2D를 받는다
+SamplerState SampleType;
+
+
+//////////////
+// TYPEDEFS //
+//////////////
+struct PixelInputType
+{
+    float4 position : SV_POSITION;
+    float2 tex : TEXCOORD0;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Pixel Shader
+////////////////////////////////////////////////////////////////////////////////
+float4 MultiTexturePixelShader(PixelInputType input) : SV_TARGET
+{
+	float4 color1;
+    float4 color2;
+    float4 lightColor;
+    float4 blendColor;
+
+
+    // 첫 번째 텍스처에서 픽셀 색상을 가져옵니다.
+    color1 = shaderTextures[0].Sample(SampleType, input.tex);
+
+    // 두 번째 텍스처에서 픽셀 색상을 가져옵니다.
+    color2 = shaderTextures[1].Sample(SampleType, input.tex);
+
+    // 세 번째 텍스처에서 픽셀 색상을 가져옵니다.
+    lightColor = shaderTextures[2].Sample(SampleType, input.tex);
+
+
+    // 두 픽셀을 혼합하고 감마 값을 곱합니다.
+    blendColor = color1 * color2 * 2.2f; //두픽셀값을 곱하고 감마보정값을 곱한다
+    
+    // 최종 색상을 채 웁니다.
+    blendColor = saturate(blendColor); //최종적으로 곱해진 픽셀을 0 ~ 1사이값으로 보간하기위해서 Saturate를 사용한다
+
+    //blendColor *= lightColor;
+    return blendColor;
+}
