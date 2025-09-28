d79c3a33a12e942e491a42e001c09f724896cafe
선용 <ckdlscjs@naver.com>
2025-09-29 04:50:03 +0900

    #FEAT RasterTek23.Fog(선형, 지수 안개)

---
+++ C_CPP/RasterTek23/GraphicsClass.cpp
@@ -0,0 +1,170 @@
+#include "stdafx.h"
+#include "d3dclass.h"
+#include "cameraclass.h"
+#include "modelclass.h"
+#include "FogShaderClass.h"
+#include "graphicsclass.h"
+
+
+GraphicsClass::GraphicsClass()
+{
+}
+
+
+GraphicsClass::GraphicsClass(const GraphicsClass& other)
+{
+}
+
+
+GraphicsClass::~GraphicsClass()
+{
+}
+
+
+bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
+{
+	// Direct3D 객체 생성
+	m_Direct3D = new D3DClass;
+	if (!m_Direct3D)
+	{
+		return false;
+	}
+
+	// Direct3D 객체 초기화
+	if (!m_Direct3D->Initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR))
+	{
+		MessageBox(hwnd, L"Could not initialize Direct3D.", L"Error", MB_OK);
+		return false;
+	}
+
+	// m_Camera 객체 생성
+	m_Camera = new CameraClass;
+	if (!m_Camera)
+	{
+		return false;
+	}
+
+	// 모델 객체 생성
+	m_Model = new ModelClass;
+	if (!m_Model)
+	{
+		return false;
+	}
+
+	// 모델 객체 초기화
+	if (!m_Model->Initialize(m_Direct3D->GetDevice(), L"../RasterTek23/data/seafloor.dds", "../RasterTek23/data/cube.txt"))
+	{
+		MessageBox(hwnd, L"Could not initialize the model object.", L"Error", MB_OK);
+		return false;
+	}
+
+	// 안개 쉐이더 객체를 생성한다.
+	m_FogShader = new FogShaderClass;
+	if(!m_FogShader)
+	{
+		return false;
+	}
+
+	// 안개 쉐이더 객체를 초기화 합니다.
+	if(!m_FogShader->Initialize(m_Direct3D->GetDevice(), hwnd))
+	{
+		MessageBox(hwnd, L"Could not initialize the fog shader object.", L"Error", MB_OK);
+		return false;
+	}
+
+	return true;
+}
+
+
+void GraphicsClass::Shutdown()
+{
+	// 안개 쉐이더 객체 반환
+	if(m_FogShader)
+	{
+		m_FogShader->Shutdown();
+		delete m_FogShader;
+		m_FogShader = 0;
+	}
+
+	// 모델 객체 반환
+	if (m_Model)
+	{
+		m_Model->Shutdown();
+		delete m_Model;
+		m_Model = 0;
+	}
+
+	// m_Camera 객체 반환
+	if (m_Camera)
+	{
+		delete m_Camera;
+		m_Camera = 0;
+	}
+
+	// Direct3D 객체 반환
+	if (m_Direct3D)
+	{
+		m_Direct3D->Shutdown();
+		delete m_Direct3D;
+		m_Direct3D = 0;
+	}
+}
+
+
+bool GraphicsClass::Frame()
+{
+	// 카메라 위치 설정
+	m_Camera->SetPosition(0.0f, 0.0f, -250.0f);
+
+	return true;
+}
+
+
+bool GraphicsClass::Render()
+{
+	// 안개의 색을 회색으로 설정합니다.
+	float fogColor = 0.5f;
+
+	// 안개의 시작과 끝을 설정합니다.
+	float fogStart = 0.0f;
+	float fogEnd = 250.0f;
+	float fogDensity = 0.002f;
+
+	// 씬을 그리기 위해 버퍼를 지웁니다
+	m_Direct3D->BeginScene(0.0f, 0.0f, 0.0f, 1.0f);
+
+	// 카메라의 위치에 따라 뷰 행렬을 생성합니다
+	m_Camera->Render();
+
+	// 카메라 및 d3d 객체에서 월드, 뷰 및 투영 행렬을 가져옵니다
+	XMMATRIX worldMatrix, viewMatrix, projectionMatrix;
+	m_Camera->GetViewMatrix(viewMatrix);
+	m_Direct3D->GetWorldMatrix(worldMatrix);
+	m_Direct3D->GetProjectionMatrix(projectionMatrix);
+
+	// 각 프레임의 rotation 변수를 업데이트합니다.
+	static float rotation = 0.0f;
+	rotation += (float)XM_PI * 0.0025f;
+	if(rotation > 360.0f)
+	{
+		rotation -= 360.0f;
+	}
+
+	// 회전 값으로 월드 행렬을 회전합니다.
+	worldMatrix = worldMatrix * XMMatrixRotationY(rotation);
+
+	// 모델 버텍스와 인덱스 버퍼를 그래픽 파이프 라인에 배치하여 렌더링 합니다.
+	m_Model->Render(m_Direct3D->GetDeviceContext());
+
+	// 안개 쉐이더로 모델을 렌더링합니다.	
+	if(!m_FogShader->Render(m_Direct3D->GetDeviceContext(), m_Model->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, 
+								 m_Model->GetTexture(), fogStart, fogEnd, fogDensity))
+	{
+		return false;
+	}
+
+	// 버퍼의 내용을 화면에 출력합니다
+	m_Direct3D->EndScene();
+
+	return true;
+}
